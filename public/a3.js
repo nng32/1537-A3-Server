77dc93d2d2035a443fcdd14fbff41127cac823fb
nng32 <nng32@my.bcit.ca>
2022-04-12 21:24:44 -0700

    Complete A3

---
+++ public/a3.js
@@ -0,0 +1,121 @@
+unicornData = null;
+
+function process_response(data) {
+    console.log(data);
+    unicornData = data;
+    display();
+}
+
+function findUnicornByName() {
+    console.log("findUnicornByName() was called")
+    $.ajax(
+        {
+            url: "http://localhost:5000/findUnicornByName",
+            type: "POST",
+            data: {
+                "unicornName": $("#unicornName").val()
+            },
+            success: process_response
+        }
+    )
+    resetFilters();
+    $('#filters').show();
+}
+
+
+function findUnicornByWeight() {
+    console.log("findUnicornByWeight() was called")
+    $.ajax(
+        {
+            url: "http://localhost:5000/findUnicornByWeight",
+            type: "POST",
+            data: {
+                "unicornWeightLower": parseInt($("#lowerWeight").val()),
+                "unicornWeightUpper": parseInt($("#upperWeight").val())
+            },
+            success: process_response
+        }
+    )
+    resetFilters();
+    $('#filters').show();
+}
+
+function findUnicornByFood() {
+    appleIsChecked = "unchecked";
+    carrotIsChecked = "unchecked";
+
+    if($('#apple').is(":checked")) {
+        appleIsChecked = "checked";
+    }
+
+    if($('#carrot').is(":checked")) {
+        carrotIsChecked = "checked";
+    }
+
+    console.log("findUnicornByFood() was called")
+    $.ajax(
+        {
+            url: "http://localhost:5000/findUnicornByFood",
+            type: "POST",
+            data: {
+                "appleIsChecked": appleIsChecked,
+                "carrotIsChecked": carrotIsChecked
+            },
+            success: process_response
+        }
+    )
+    resetFilters();
+    $('#filters').show();
+}
+
+function display() {
+    dataObject = JSON.parse(unicornData);
+
+    nameIsChecked = false;
+    weightIsChecked = false;
+
+    if ($('#unicornNameFilter').is(":checked")) {
+        nameIsChecked = "checked";
+    }
+
+    if ($('#unicornWeightFilter').is(":checked")) {
+        weightIsChecked = "checked";
+    }
+
+    displayResults = dataObject.map(unicorn => {
+        result = [];
+
+        if (nameIsChecked) {
+            result.push(unicorn['name']);
+        }
+
+        if (weightIsChecked) {
+            result.push(unicorn['weight']);
+        }
+
+        if (!nameIsChecked && !weightIsChecked) {
+            result.push(JSON.stringify(unicorn));
+        }
+
+        return result;
+    })
+
+    $('#result').html(displayResults.toString());
+}
+
+function resetFilters() {
+    $('#unicornNameFilter').prop('checked', false);
+    $('#unicornWeightFilter').prop('checked', false);    
+}
+
+function setup() {
+    $('#filters').hide();
+    resetFilters();
+    
+    $('#findUnicornByName').click(findUnicornByName);
+    $('#findUnicornByFood').click(findUnicornByFood);
+    $('#findUnicornByWeight').click(findUnicornByWeight);
+    $('#filter').click(display);
+}
+
+$(document).ready(setup);
---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
+
+const unicornSchema = new mongoose.Schema({
+    name: String,
+    weight: Number,
+    dob: Date,
+    loves: [String],
+    gender: String
+});
+
+const unicornModel = mongoose.model("unicorns", unicornSchema);
+
+app.listen(process.env.PORT || 5000, err => {
+    if (err) console.log(err);
+})
+
+app.use(express.static(`public`));
+
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+app.post("/findUnicornByName", (req, res) => {
+    console.log("request has been received");
+    // res.send("Request received");
+
+    unicornModel.find({
+        name: req.body.unicornName
+    }, (err, unicorns) => {
+        if (err) {
+            console.log("Error occurred");
+        } else {
+            console.log("On time, on course, and on target.");
+            console.log(unicorns);
+        }
+        res.send(JSON.stringify(unicorns));
+    });
+})
+
+app.post("/findUnicornByWeight", (req, res) => {
+    console.log("request has been received");
+    // res.send("Request received");
+
+    unicornModel.find({
+        weight: {
+            $gt: req.body.unicornWeightLower,
+            $lt: req.body.unicornWeightUpper
+        }
+    }, (err, unicorns) => {
+        if (err) {
+            console.log("Error occurred");
+        } else {
+            console.log("On time, on course, and on target.");
+            console.log(unicorns);
+        }
+        res.send(JSON.stringify(unicorns));
+    });
+})
+
+app.post("/findUnicornByFood", (req, res) => {
+    console.log("request has been received");
+    // res.send("Request received");
+
+    appleIsChecked = req.body.appleIsChecked;
+    carrotIsChecked = req.body.carrotIsChecked;
+    someList = [];
+    if (appleIsChecked == 'checked') {
+        someList.push({loves: 'apple'});
+    }
+    if (carrotIsChecked == 'checked') {
+        someList.push({loves: 'carrot'});
+    }
+
+    console.log(someList);
+
+    unicornModel.find({
+        $and: someList
+    }, (err, unicorns) => {
+        if (err) {
+            console.log("Error occurred");
+        } else {
+            console.log("On time, on course, and on target.");
+            console.log(unicorns);
+        }
+        res.send(JSON.stringify(unicorns));
+    });
+})
+
+mongoose.connect("mongodb://localhost:27017/test",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology:  true
+});
